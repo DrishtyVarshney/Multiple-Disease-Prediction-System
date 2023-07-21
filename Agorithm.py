bfe899ef517a382a2835a28e7fe150dc5f2641fb
DrishtyVarshney <100081706+DrishtyVarshney@users.noreply.github.com>
2023-07-21 21:51:36 +0530

    Create Agorithm.py

---
+++ Agorithm.py
@@ -0,0 +1,44 @@
+symptoms = X.columns.values
+
+# Creating a symptom index dictionary to encode the
+# input symptoms into numerical form
+symptom_index = {}
+for index, value in enumerate(symptoms):
+	symptom = " ".join([i.capitalize() for i in value.split("_")])
+	symptom_index[symptom] = index
+
+data_dict = {
+	"symptom_index":symptom_index,
+	"predictions_classes":encoder.classes_
+}
+
+
+def predictDisease(symptoms):
+	symptoms = symptoms.split(",")
+	
+	
+	input_data = [0] * len(data_dict["symptom_index"])
+	for symptom in symptoms:
+		index = data_dict["symptom_index"][symptom]
+		input_data[index] = 1
+		
+	
+	input_data = np.array(input_data).reshape(1,-1)
+	
+	
+	rf_prediction = data_dict["predictions_classes"][final_rf_model.predict(input_data)[0]]
+	nb_prediction = data_dict["predictions_classes"][final_nb_model.predict(input_data)[0]]
+	svm_prediction = data_dict["predictions_classes"][final_svm_model.predict(input_data)[0]]
+	
+	# making final prediction by taking mode of all predictions
+	final_prediction = mode([rf_prediction, nb_prediction, svm_prediction])[0][0]
+	predictions = {
+		"rf_model_prediction": rf_prediction,
+		"naive_bayes_prediction": nb_prediction,
+		"svm_model_prediction": svm_prediction,
+		"final_prediction":final_prediction
+	}
+	return predictions
+
+# Testing the function
+print(predictDisease("Itching,Skin Rash,Nodal Skin Eruptions"))
